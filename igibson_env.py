fb525842cf6581e99e9ddb06bf086b54623dc961
qian <luoqian19961224@gmail.com>
2021-09-06 13:54:15 -0400

    test interactive_nav

--- igibson_env.py
+++ igibson_env.py
@@ -64,7 +64,7 @@ WEIGHTS_PATH = '/nethome/qluo49/iGibsonChallenge2021/ckpt.36.json'
 def load_model(weights_path, dim_actions): # DON'T CHANGE
     depth_256_space = SpaceDict({
         # 'depth': spaces.Box(low=0., high=1., shape=(260,348,1)),
-        'depth': spaces.Box(low=0., high=1., shape=(256,256,1)),
+        'depth': spaces.Box(low=0., high=1., shape=(240,320,1)),
         'pointgoal_with_gps_compass': spaces.Box(
             low=np.finfo(np.float32).min,
             high=np.finfo(np.float32).max,
