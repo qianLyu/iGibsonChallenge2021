69aa863c13638f5259594169c2d15c389826a19f
qian <luoqian19961224@gmail.com>
2021-09-10 11:24:20 -0400

    int

--- trained_agent.py
+++ trained_agent.py
@@ -44,7 +44,7 @@ import matplotlib.image as mp
 
 
 # WEIGHTS_PATH = 'habitat_cont_2/habitat_cont/gaussian_noslide_30deg_63_skyfail.json'
-WEIGHTS_PATH = '/nethome/qluo49/iGibsonChallenge2021/ckpt.23.json'
+WEIGHTS_PATH = '/nethome/qluo49/iGibsonChallenge2021/ckpt.72.json'
 
 def load_model(weights_path, dim_actions): # DON'T CHANGE
     depth_256_space = SpaceDict({
